05f7145f763ba94e922884d404f0808b97909b9d
Saturn-98 <ishaan.3098@gmail.com>
2025-09-09 13:11:22 +0530

    structural directive

---
+++ src/app/auth/auth.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, effect, ElementRef, inject, input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Permission } from './auth.model';
+import { AuthService } from './auth.service';
+
+@Directive({
+  selector: '[appAuth]',
+  standalone: true
+})
+export class AuthDirective {
+   userType = input.required<Permission>({alias: 'appAuth'});
+  private authService = inject(AuthService);
+  private refTemplate = inject(TemplateRef);
+  private viewContainerRef = inject(ViewContainerRef);
+
+  constructor() { 
+    effect(() =>{
+      if(this.authService.activePermission() === this.userType()){
+        this.viewContainerRef.createEmbeddedView(this.refTemplate);
+      }else {
+        this.viewContainerRef.clear();
+      }
+    });
+  }
+
+}
